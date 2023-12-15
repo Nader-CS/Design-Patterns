6e4675b4c0a05631b24094c631aa47b0ff69b096
Nader <naderabdalhamed93@gmail.com>
2023-12-15 03:12:21 +0200

    adding memnto pattern

---
+++ 1- memnto/src/main/Main.java
@@ -0,0 +1,28 @@
+
+package main;
+
+import memento.Editor;
+import memento.History;
+
+
+public class Main {
+    public static void main(String[] args){
+        Editor editor = new Editor();
+        History history = new History();
+        
+        editor.setContent("a");
+        history.push(editor.createState());
+        
+        editor.setContent("b");
+        history.push(editor.createState());
+        
+        editor.setContent("c");
+        
+        editor.restore(history.pop());
+        System.out.println(editor.getContent()); //prints (b)
+        
+        editor.restore(history.pop());
+        System.out.println(editor.getContent());//prints (a)
+    }
+    
+}
